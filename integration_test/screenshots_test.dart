d1d318cd26f7bf8252683982047acf45320bb3bc
CrispStrobe <cze+github@mailbox.org>
2026-09-12 08:41:38 +0200

    Fix await app.main compile error in screenshots_test

--- integration_test/screenshots_test.dart
+++ integration_test/screenshots_test.dart
@@ -57,7 +57,7 @@ void main() {
   }
 
   testWidgets('capture store screenshots', (tester) async {
-    await app.main();
+    app.main([]);
     await hold(tester, ms: 2500);
 
     // 1) Transcription/Home screen
